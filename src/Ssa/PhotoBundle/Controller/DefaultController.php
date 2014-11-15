313e73dac561094f8d5793f84e2f660adcdc97ae
974steph <stephane.sautron@gmail.com>
2014-11-15 22:38:44 +0100

    fait a la main

--- src/Ssa/PhotoBundle/Controller/DefaultController.php
+++ src/Ssa/PhotoBundle/Controller/DefaultController.php
@@ -12,6 +12,8 @@ use Symfony\Component\Security\Acl\Domain\RoleSecurityIdentity;
 use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
 use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
 use Symfony\Component\Security\Acl\Permission\MaskBuilder;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultController extends Controller
 {
@@ -51,7 +53,7 @@ class DefaultController extends Controller
         $chemin=$this->get('kernel')->getRootDir();
         $imagesArr	= array();
 
-        $avalancheService = $this->get('imagine.cache.path.resolver');
+  
         $finder = new Finder();
         $finder->files()
                 ->in($chemin.'/../web/images/'.$book->getPath())
@@ -60,12 +62,138 @@ class DefaultController extends Controller
 
         foreach ($finder as $files)
         {
-                $path= $avalancheService->getBrowserPath('/images/'.$book->getPath().'/'.$files->getRelativePathname(), 'thumb');
-                $imagesArr[] = array('src' => $path,
-                                     'alt'	=> '/images/'.$book->getPath().'/'.$files->getRelativePathname(),
+                $uriImage = $this->get('router')->generate('ssa_photo_get',   array('fichier' => $book->getPath().'/'.$files->getRelativePathname() ));
+                $uriThumb = $this->get('router')->generate('ssa_photo_cache', array('fichier' => $book->getPath().'/'.$files->getRelativePathname() ));
+               
+                $imagesArr[] = array('src'      => $uriThumb,
+                                     'alt'	=> $uriImage,
                                      'desc'	=> "");
         }
 
         return new JsonResponse($imagesArr);
     }
+    
+    public function imageAction($fichier)
+    {
+       
+        $chemin=$this->get('kernel')->getRootDir();
+        
+        $imgpath = $chemin .'/../web/images/'. $fichier;
+
+        list($header, $content) = $this->renderImage($imgpath);
+        
+        return new Response($content, 200, $header);
+        
+     
+    }
+    
+    
+    public function renderImage($imgpath)
+    {
+    
+        // Get the mimetype for the file
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);  // return mime type ala mimetype extension
+        $mime_type = finfo_file($finfo, $imgpath);
+        finfo_close($finfo);
+        /*
+        ob_start();
+        imagejpeg($img, null, 100);
+        $image = base64_encode(ob_get_contents());
+        ob_get_clean();
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'image/jpeg');
+        $response->setContent($image);
+        return $response;
+        */
+        ob_start();
+        switch ($mime_type){
+            case "image/jpeg":
+                // Set the content type header - in this case image/jpg
+               // header('Content-Type: image/jpeg');
+                 $headers = array('Content-Type'     => 'image/jpeg');
+                // Get image from file
+                $img = imagecreatefromjpeg($imgpath);
+                // Output the image
+                imagejpeg($img);
+                break;
+            case "image/png":
+                // Set the content type header - in this case image/png
+               // header('Content-Type: image/png');
+                 $headers = array('Content-Type'     => 'image/png');
+                // Get image from file
+                $img = imagecreatefrompng($imgpath);
+                // integer representation of the color black (rgb: 0,0,0)
+                $background = imagecolorallocate($img, 0, 0, 0);
+                // removing the black from the placeholder
+                imagecolortransparent($img, $background);
+                // turning off alpha blending (to ensure alpha channel information
+                // is preserved, rather than removed (blending with the rest of the
+                // image in the form of black))
+                imagealphablending($img, false);
+                // turning on alpha channel information saving (to ensure the full range
+                // of transparency is preserved)
+                imagesavealpha($img, true);
+                // Output the image
+                imagepng($img);
+                break;
+            case "image/gif":
+                // Set the content type header - in this case image/gif
+                //header('Content-Type: image/gif');
+                   $headers = array('Content-Type'     => 'image/gif');
+                // Get image from file
+                $img = imagecreatefromgif($imgpath);
+                // integer representation of the color black (rgb: 0,0,0)
+                $background = imagecolorallocate($img, 0, 0, 0);
+                // removing the black from the placeholder
+                imagecolortransparent($img, $background);
+                // Output the image
+                imagegif($img);
+                break;
+        }
+        
+       $image= ob_get_clean();
+        // Free up memory
+        imagedestroy($img);
+        
+            return array($headers,$image);
+    }
+    
+    public function thumbAction($fichier)
+    {
+           
+        $chemin=$this->get('kernel')->getRootDir();
+        $imgpathori = $chemin .'/../web/images/'. $fichier;
+        $imgpath = $chemin .'/../web/cache/thumb/'. $fichier;
+        
+        if (!file_exists($imgpath))
+        {          
+
+            $directory = dirname($imgpath);
+            $file = basename($imgpath);
+            
+            if (!file_exists($directory) && !is_dir($directory)) 
+            {
+                mkdir($directory, 0755, true);      
+            } 
+                     
+            $cmd=sprintf("/usr/bin/convert '%s' -resize 120x90 '%s'",$imgpathori,$imgpath);
+            exec($cmd);
+        
+        }
+        list($header, $content) = $this->renderImage($imgpath);
+       
+        return new Response($content, 200, $header);
+       
+    }
+    
+    
+    
+    
+    
+
+
+
+
+
 }
